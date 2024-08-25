eebc97abe9cc7e1a2847e2b500f7f317815ce77d
kareemAlqubati <salamkareem792@gmail.com>
2024-08-25 09:41:54 +0300

    Compelet

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+const userName = 'YourName';
+const headerText = `${userName} is the champion`;
+
+newHeader.textContent = headerText;
+
+document.body.appendChild(newHeader);
